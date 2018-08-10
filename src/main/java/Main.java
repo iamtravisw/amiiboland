635b11e89b5cd17a97798da49003845fac23f1f5
Unknown <iamtravisw@gmail.com>
2018-08-10 03:56:14 +0000

    initial project setup.

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+class Main {
+
+    public static void main(String[] args) {
+
+        // Instance Variables
+        String dbURL = System.getenv("DB_URL");
+        String dbUser = System.getenv("DB_USER");
+        String dbPassword = System.getenv("DB_PASSWORD");
+        
+
+
+    }
+}
