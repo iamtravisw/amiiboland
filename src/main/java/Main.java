81d60f23c69772f56b25e8158a80617cb29ef82e
Unknown <iamtravisw@gmail.com>
2018-08-11 08:29:27 +0000

    finished wishlist logic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -112,18 +112,27 @@ public class Main {
                         psCheckDelete.setInt(2, userID);                             // UserID
                         ResultSet rsDelete = psCheckDelete.executeQuery();              // Execute
 
-                        while (rsDelete.next()) {
-                            int collectionID = rsDelete.getInt("CollectionID");
 
-                            // sql delete statement
-                            String delete = "delete from Collection where CollectionID = ?";
-                            PreparedStatement psDelete = conn.prepareStatement(delete);
-                            psDelete.setInt(1, collectionID);
-                            psDelete.executeUpdate();
+                        // If the row does not exist, insert a new row for this user
+                        if (!rsDelete.isBeforeFirst()) {
+                            System.out.println("Data does not exist... Nothing to do.");
+                        } else {
+                            while (rsDelete.next()) {
+                                int collectionID = rsDelete.getInt("CollectionID");
+                                System.out.println("The row exists as CollectionID: " + collectionID + ". Updating database...");
 
-                            System.out.println("Deleted CollectionID: " + collectionID);
+                                // update the data
+                                PreparedStatement psDelete = conn.prepareStatement(
+                                        "UPDATE Collection SET Collected = ?, ModUser = ?, ModDate = ? WHERE CollectionID = ?");
+
+                                // set the preparedstatement parameters
+                                psDelete.setString(1, "N");              // Collected
+                                psDelete.setInt(2, userID);                 // ModUser
+                                psDelete.setString(3, moddate.toString());  // ModDate
+                                psDelete.setInt(4, collectionID);           // CollectionID
+                                psDelete.executeUpdate();                      // Execute
+                            }
                         }
-
 
                         // ==================================================
                         // Add Favorite Logic
@@ -173,10 +182,175 @@ public class Main {
                                     psFaveUpdate.setString(3, moddate.toString()); // ModDate
                                     psFaveUpdate.setInt(4, collectionID);           // CollectionID
                                     psFaveUpdate.executeUpdate();                    // Execute
-                                    System.out.println("Updated row...");
 
                             }
                         }
+
+                        // ==================================================
+                        // Remove Favorite Logic
+                        // ==================================================
+
+                    } else if (subMenu == 4) {
+
+                        // Check to see if the row already exists before inserting any new rows
+                        String checkExistsFaveRemove = "SELECT CollectionID FROM Collection WHERE AmiiboID = ? AND UserID = ? LIMIT 1";
+                        PreparedStatement psAddFaveRemove = conn.prepareStatement(checkExistsFaveRemove);
+                        psAddFaveRemove.setInt(1, menu);                                 // AmiiboID
+                        psAddFaveRemove.setInt(2, userID);                               // UserID
+                        ResultSet rsFaveRemove = psAddFaveRemove.executeQuery();               // Execute
+
+                        // If the row does not exist, insert a new row for this user
+                        if (!rsFaveRemove.isBeforeFirst()) {
+                            System.out.println("Data does not exist... No need to add it.");
+
+                            /*
+                            System.out.println("Data does not exist... Inserting into database...");
+
+                            // sql insert statement
+                            String faveAmiiboRemove = "insert into Collection (AmiiboID, UserID, Favorited, ModUser, ModDate, AddUser, AddDate)"
+                                    + "values (?, ?, ?, ?, ?, ?, ?)";
+                            PreparedStatement psFaveInsertRemove = conn.prepareStatement(faveAmiiboRemove);
+
+                            // sql insert values
+                            psFaveInsertRemove.setInt(1, menu);                  // AmiiboID
+                            psFaveInsertRemove.setInt(2, userID);                 // UserID
+                            psFaveInsertRemove.setString(3, "N");             // Favorited Y/N
+                            psFaveInsertRemove.setInt(4, userID);                 // ModUser (ID)
+                            psFaveInsertRemove.setString(5, moddate.toString()); // ModDate
+                            psFaveInsertRemove.setInt(6, userID);                 // AddUser (ID)
+                            psFaveInsertRemove.setString(7, adddate.toString()); // AddDate
+                            psFaveInsertRemove.execute();                           // Execute
+                            */
+
+                            // If the row does exist, simply update it
+                        } else {
+                            while (rsFaveRemove.next()) {
+                                int collectionID = rsFaveRemove.getInt("CollectionID");
+                                System.out.println("This row already exists as CollectionID: " + collectionID + ". Updating database...");
+
+                                // update the data
+                                PreparedStatement psFaveUpdateRemove = conn.prepareStatement(
+                                        "UPDATE Collection SET Favorited = ?, ModUser = ?, ModDate = ? WHERE CollectionID = ?");
+
+                                // set the preparedstatement parameters
+                                psFaveUpdateRemove.setString(1, "N");             // Favorited
+                                psFaveUpdateRemove.setInt(2, userID);                 // ModUser
+                                psFaveUpdateRemove.setString(3, moddate.toString()); // ModDate
+                                psFaveUpdateRemove.setInt(4, collectionID);           // CollectionID
+                                psFaveUpdateRemove.executeUpdate();                    // Execute
+
+                            }
+                        }
+
+                        // ==================================================
+                        // Add Wish List Logic
+                        // ==================================================
+
+                    } else if (subMenu == 5) {
+
+                        // Check to see if the row already exists before inserting any new rows
+                        String checkExistsWishList = "SELECT CollectionID FROM Collection WHERE AmiiboID = ? AND UserID = ? LIMIT 1";
+                        PreparedStatement psAddWishList = conn.prepareStatement(checkExistsWishList);
+                        psAddWishList.setInt(1, menu);                                 // AmiiboID
+                        psAddWishList.setInt(2, userID);                               // UserID
+                        ResultSet rsAddWishList = psAddWishList.executeQuery();               // Execute
+
+                        // If the row does not exist, insert a new row for this user
+                        if (!rsAddWishList.isBeforeFirst()) {
+                            System.out.println("Data does not exist... Inserting into database...");
+
+                            // sql insert statement
+                            String faveAmiibo = "insert into Collection (AmiiboID, UserID, WishList, ModUser, ModDate, AddUser, AddDate)"
+                                    + "values (?, ?, ?, ?, ?, ?, ?)";
+                            PreparedStatement psWishInsert = conn.prepareStatement(faveAmiibo);
+
+                            // sql insert values
+                            psWishInsert.setInt(1, menu);                  // AmiiboID
+                            psWishInsert.setInt(2, userID);                 // UserID
+                            psWishInsert.setString(3, "Y");             // WishList Y/N
+                            psWishInsert.setInt(4, userID);                 // ModUser (ID)
+                            psWishInsert.setString(5, moddate.toString()); // ModDate
+                            psWishInsert.setInt(6, userID);                 // AddUser (ID)
+                            psWishInsert.setString(7, adddate.toString()); // AddDate
+                            psWishInsert.execute();                           // Execute
+
+                            // If the row does exist, simply update it
+                        } else {
+                            while (rsAddWishList.next()) {
+                                int collectionID = rsAddWishList.getInt("CollectionID");
+                                System.out.println("This row already exists as CollectionID: " + collectionID + ". Updating database...");
+
+                                // update the data
+                                PreparedStatement psWishUpdate = conn.prepareStatement(
+                                        "UPDATE Collection SET WishList = ?, ModUser = ?, ModDate = ? WHERE CollectionID = ?");
+
+                                // set the preparedstatement parameters
+                                psWishUpdate.setString(1, "Y");             // WishList
+                                psWishUpdate.setInt(2, userID);                 // ModUser
+                                psWishUpdate.setString(3, moddate.toString()); // ModDate
+                                psWishUpdate.setInt(4, collectionID);           // CollectionID
+                                psWishUpdate.executeUpdate();                    // Execute
+
+                            }
+                        }
+
+                        // ==================================================
+                        // Remove Wish List Logic
+                        // ==================================================
+
+                    } else if (subMenu == 6) {
+
+                        // Check to see if the row already exists before inserting any new rows
+                        String checkExistsWishRemove = "SELECT CollectionID FROM Collection WHERE AmiiboID = ? AND UserID = ? LIMIT 1";
+                        PreparedStatement psWishRemove = conn.prepareStatement(checkExistsWishRemove);
+                        psWishRemove.setInt(1, menu);                                 // AmiiboID
+                        psWishRemove.setInt(2, userID);                               // UserID
+                        ResultSet rsWishRemove = psWishRemove.executeQuery();         // Execute
+
+                        // If the row does not exist, insert a new row for this user
+                        if (!rsWishRemove.isBeforeFirst()) {
+                            System.out.println("Data does not exist... No need to add it.");
+
+                            /*
+                            System.out.println("Data does not exist... Inserting into database...");
+
+                            // sql insert statement
+                            String wishAmiiboRemove = "insert into Collection (AmiiboID, UserID, Favorited, ModUser, ModDate, AddUser, AddDate)"
+                                    + "values (?, ?, ?, ?, ?, ?, ?)";
+                            PreparedStatement psWishInsertRemove = conn.prepareStatement(wishAmiiboRemove);
+
+                            // sql insert values
+                            psWishInsertRemove.setInt(1, menu);                  // AmiiboID
+                            psWishInsertRemove.setInt(2, userID);                 // UserID
+                            psWishInsertRemove.setString(3, "N");             // Favorited Y/N
+                            psWishInsertRemove.setInt(4, userID);                 // ModUser (ID)
+                            psWishInsertRemove.setString(5, moddate.toString()); // ModDate
+                            psWishInsertRemove.setInt(6, userID);                 // AddUser (ID)
+                            psWishInsertRemove.setString(7, adddate.toString()); // AddDate
+                            psWishInsertRemove.execute();                           // Execute
+                            */
+
+                            // If the row does exist, simply update it
+                        } else {
+                            while (rsWishRemove.next()) {
+                                int collectionID = rsWishRemove.getInt("CollectionID");
+                                System.out.println("This row already exists as CollectionID: " + collectionID + ". Updating database...");
+
+                                // update the data
+                                PreparedStatement psFaveUpdateRemove = conn.prepareStatement(
+                                        "UPDATE Collection SET WishList = ?, ModUser = ?, ModDate = ? WHERE CollectionID = ?");
+
+                                // set the preparedstatement parameters
+                                psFaveUpdateRemove.setString(1, "N");             // WishList
+                                psFaveUpdateRemove.setInt(2, userID);                 // ModUser
+                                psFaveUpdateRemove.setString(3, moddate.toString()); // ModDate
+                                psFaveUpdateRemove.setInt(4, collectionID);           // CollectionID
+                                psFaveUpdateRemove.executeUpdate();                    // Execute
+
+                            }
+                        }
+                    } else {
+                        System.out.println("You must select option 1-6.");
                     }
                 }
 
