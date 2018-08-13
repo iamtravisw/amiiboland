e7af18fd602ab8e9c3bc4d93ea8dae955987ce97
Unknown <iamtravisw@gmail.com>
2018-08-13 01:17:23 +0000

    Changed switch to Gem, rearranged icons. Added forms for checkboxes, hid ugly checkboxes using fontawesome replacements

--- src/main/resources/public/amiiboland.js
+++ src/main/resources/public/amiiboland.js
@@ -1,24 +1,32 @@
 $(document).ready(function(){
-  
+
     $(".amiibo-image").mouseover(function() {
     console.log("amiibo-image has been moused over");
     $(".overlay").removeClass("is-invisible");
   });
 
-  
-  $(".overlay").mouseleave(function() {
+    $(".overlay").mouseleave(function() {
     console.log("mouse moved off amiibo-image");
      $(".overlay").addClass("is-invisible");
   });
 
     $(".star").click(function() {
-        console.log("Wish List clicked (star)");
+        console.log("'Mine' clicked (Star)");
         $(".star").toggleClass("has-text-warning");
     });
 
+
     $(".heart").click(function() {
-        console.log("Favorite clicked (heart)");
+        console.log("'Love' clicked (Heart)");
         $(".heart").toggleClass("has-text-danger");
     });
 
+    $(".gem").click(function() {
+        console.log("'Want' clicked (Gem)");
+        $(".gem").toggleClass("has-text-info");
+    });
+
 });
+
+
+if($('#termsCheck').prop('checked')){}
