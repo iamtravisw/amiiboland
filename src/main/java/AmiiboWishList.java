732a90153bb13b9e79529f5f4e7431c97c2fd526
Travis Woodward <iamtravisw@gmail.com>
2018-10-10 03:01:19 +0000

    Removed Add/Remove, Favorite/Unfavorite and WishList/ UnWishList classes. Created three less granular classes: AmiiboCollect, AmiiboFavorite and AmiiboWishlist which will handle adding and removing.

--- src/main/java/AmiiboWishList.java
+++ src/main/java/AmiiboWishList.java
@@ -1,9 +1,9 @@
-import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Date;
-import java.sql.Connection;
-import java.sql.DriverManager;
 
-public class AddWishList {
+public class AmiiboWishList {
+
     // Variables
     public int userID = 1;
     public int amiiboID;
@@ -21,8 +21,7 @@ public class AddWishList {
     public int getUserID() {
         return userID;
     }
-
-    public void main(String[] args) {
+    public void wishlistAmiibo(String[] args) {
 
         ConnHelper connHelper = new ConnHelper();
         Date addDate = new Date();
@@ -83,6 +82,57 @@ public class AddWishList {
                 }
             }
             connHelper.db().close();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+    public void unwishstAmiibo(String[] args) {
+
+        ConnHelper connHelper = new ConnHelper();
+        Date modDate = new Date();
+        int amiiboID = this.amiiboID;
+        int userID = this.userID;
+
+        System.out.println("UserID is: " +userID);
+
+        try {
+            // Check to see if the row already exists before inserting any new rows
+            String checkExistsWishRemove = "SELECT CollectionID FROM Collection WHERE AmiiboID = ? AND UserID = ? LIMIT 1";
+            PreparedStatement psWishRemove = connHelper.db().prepareStatement(checkExistsWishRemove);
+            psWishRemove.setInt(1, amiiboID);                                 // AmiiboID
+            psWishRemove.setInt(2, userID);                               // UserID
+            ResultSet rsWishRemove = psWishRemove.executeQuery();         // Execute
+
+            System.out.println("RemoveWishList class has an ID of: " + this.amiiboID);
+
+            // If the row does not exist, do nothing
+            if (!rsWishRemove.isBeforeFirst()) {
+                System.out.println("Data does not exist... No need to add it.");
+
+
+                // If the row does exist, simply update it
+            } else {
+                while (rsWishRemove.next()) {
+                    int collectionID = rsWishRemove.getInt("CollectionID");
+                    System.out.println("This row already exists as CollectionID: " + collectionID + ". Updating database...");
+
+                    // update the data
+                    PreparedStatement psFaveUpdateRemove = connHelper.db().prepareStatement(
+                            "UPDATE Collection SET WishList = ?, ModUser = ?, ModDate = ? WHERE CollectionID = ?");
+
+                    // set the preparedstatement parameters
+                    psFaveUpdateRemove.setString(1, "N");             // WishList
+                    psFaveUpdateRemove.setInt(2, userID);                 // ModUser
+                    psFaveUpdateRemove.setString(3, modDate.toString()); // ModDate
+                    psFaveUpdateRemove.setInt(4, collectionID);           // CollectionID
+                    psFaveUpdateRemove.executeUpdate();                    // Execute
+
+                    System.out.println("RemoveWishList class has an ID of: " + this.amiiboID);
+                }
+                connHelper.db().close();
+            }
         }
         catch (Exception e)
         {
